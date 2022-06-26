7db9688dd0f69340f73a2d63abe5e1057f290381
Eneas Baroni <eneasbaroni@gmail.com>
2022-06-26 10:43:00 -0300

    picante V04

--- src/components/SubGaleria/SubGaleria.js
+++ src/components/SubGaleria/SubGaleria.js
@@ -1,10 +1,17 @@
-import { Suspense, useState } from "react"
-
+import { useState } from "react"
+import Loader from "../Loader/Loader"
 
 const SubGaleria = ({galeria, name}) => {
+  const [loading, setLoading] = useState(true)
+  const [opacity, setOpacity] = useState(0)
   const [imgGrande, setImgGrande] = useState(false)
   const [imgGrandeSrc, setImgGrandeSrc] = useState("")
   const [visible, setVisible] = useState("0%")
+
+  const handleLoad = () => {
+    setLoading(false);
+    setOpacity(1);
+  }
 
   const handleClick = (e) => {
     console.log(e.target)
@@ -22,24 +29,27 @@ const SubGaleria = ({galeria, name}) => {
 
   return (  
     <div className="row">
-      <Suspense fallback={<h2>CARGANDO</h2>}>        
+      {loading && <Loader/>}
+      
         <div className="col-12 row imgContainer">        
-          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[0].src}`} alt={galeria[0].alt} onClick={handleClick}/>
-          <img className="galeryImg col-2" src={`/Images/${name}/${galeria[1].src}`} alt={galeria[1].alt} onClick={handleClick}/>
-          <img className="galeryImg col-2" src={`/Images/${name}/${galeria[2].src}`} alt={galeria[2].alt} onClick={handleClick}/>
-          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[3].src}`} alt={galeria[3].alt} onClick={handleClick}/>
-          <img className="galeryImg col-6" src={`/Images/${name}/${galeria[4].src}`} alt={galeria[4].alt} onClick={handleClick}/>
-          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[5].src}`} alt={galeria[5].alt} onClick={handleClick}/>
-          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[6].src}`} alt={galeria[6].alt} onClick={handleClick}/>
-          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[7].src}`} alt={galeria[7].alt} onClick={handleClick}/>
-          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[8].src}`} alt={galeria[8].alt} onClick={handleClick}/>
-          <img className="galeryImg col-5" src={`/Images/${name}/${galeria[9].src}`} alt={galeria[9].alt} onClick={handleClick}/>
-          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[10].src}`} alt={galeria[10].alt} onClick={handleClick}/>
-          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[11].src}`} alt={galeria[11].alt} onClick={handleClick}/>
-          <img className="galeryImg col-2" src={`/Images/${name}/${galeria[12].src}`} alt={galeria[12].alt} onClick={handleClick}/>
-          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[13].src}`} alt={galeria[13].alt} onClick={handleClick}/>
+          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[0].src}`} alt={galeria[0].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-2" src={`/Images/${name}/${galeria[1].src}`} alt={galeria[1].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-2" src={`/Images/${name}/${galeria[2].src}`} alt={galeria[2].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[3].src}`} alt={galeria[3].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-6" src={`/Images/${name}/${galeria[4].src}`} alt={galeria[4].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[5].src}`} alt={galeria[5].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[6].src}`} alt={galeria[6].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[7].src}`} alt={galeria[7].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[8].src}`} alt={galeria[8].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-5" src={`/Images/${name}/${galeria[9].src}`} alt={galeria[9].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-4" src={`/Images/${name}/${galeria[10].src}`} alt={galeria[10].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[11].src}`} alt={galeria[11].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-2" src={`/Images/${name}/${galeria[12].src}`} alt={galeria[12].alt} style={{opacity: opacity}} onClick={handleClick}/>
+          <img className="galeryImg col-3" src={`/Images/${name}/${galeria[13].src}`} alt={galeria[13].alt} style={{opacity: opacity}} onClick={handleClick} onLoad={handleLoad}/>
         </div>
-      </Suspense>
+      
+              
+      
 
       {imgGrande &&
         <div className="col-12 row imgGrandeContainer" style={{opacity: visible}}>
